5bd0607f9e4e28b56fa01a72931fce24f5885e5e
tk1351 <takakikitsui0111@gmail.com>
2026-02-04 09:22:54 +0900

    chore: remove standalone

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { TodoListComponent } from './features/components/todo-list/todo-list.component';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
